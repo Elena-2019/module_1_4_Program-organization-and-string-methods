3e96c36039e0cab55907df4b93fd42fee5586ba7
Elena-2019 <loy-iver@inbox.ru>
2024-08-10 02:11:08 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input('Что вы делаете в свободное время? ')
+str=my_string
+print(len(str))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(__old:=' ',__new:=''))
+print(my_string[0])
+print(my_string[-1])
